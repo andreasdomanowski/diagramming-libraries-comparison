32c1ef96eab80366434c2891d74c9d38e5b49660
Andreas Huber <andreas@andhub.de>
2021-04-15 15:10:31 +0200

    Change custom shape

--- joint-js-evaluation/src/js/customShape.js
+++ joint-js-evaluation/src/js/customShape.js
@@ -2,33 +2,17 @@ import * as joint from 'jointjs'
 
 export const customShapeIdentifier = "customShapes.CustomShape";
 
-export const CustomElement = joint.dia.Element.define(customShapeIdentifier,
-    {
-        attrs: {
-            body: {
-                refWidth: '100%',
-                refHeight: '100%',
-                strokeWidth: 2,
-                stroke: '#000000',
-                fill: '#FFFFFF'
-            },
-            label: {
-                textVerticalAnchor: 'middle',
-                textAnchor: 'middle',
-                refX: '50%',
-                refY: '50%',
-                fontSize: 14,
-                fill: '#333333'
-            }
+export const CustomElement = joint.dia.Element.define(customShapeIdentifier, {
+    markup: '<ellipse/>',
+    attrs: {
+        ellipse: {
+            fill: 'black',
+            refRx: .5,
+            refRy: .5,
+            refCx: .5,
+            refCy: .5
         }
-    }, {
-        markup: [{
-            tagName: 'rect',
-            selector: 'body',
-        }, {
-            tagName: 'text',
-            selector: 'label'
-        }]
-    });
+    }
+});
 
 
