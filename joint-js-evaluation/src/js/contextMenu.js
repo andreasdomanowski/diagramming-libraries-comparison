682fae82a25d121b924469200992c7645cd9cdc0
Andreas Huber <andreas@andhub.de>
2021-04-19 13:36:15 +0200

    Import popper

--- joint-js-evaluation/src/js/contextMenu.js
+++ joint-js-evaluation/src/js/contextMenu.js
@@ -2,7 +2,7 @@ import {graph, paper} from "./graph";
 import * as $ from "jquery";
 import * as dom_identifier from "./const/dom-identifier";
 import * as elementFactory from "./elementFactory";
-import 'bootstrap/dist/js/popper.min.js';
+import 'popper.js'
 
 // saves context menu position for avoiding busy waiting with callbacks on context menu click
 let contextMenuX = 0;
